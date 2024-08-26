e3f883b1559e721203f97eedab831410921e6faf
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-08-26 19:37:34 +0530

    jenkins-msg

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -58,11 +58,3 @@ VALIDATE $? "Starting jenkins"
 
 systemctl status jenkins &>>$LOGFILE
 VALIDATE $? "Checking Jenkins installation Status"
-
-# #!/bin/bash
-# curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-# rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-# yum install fontconfig java-17-openjdk jenkins -y
-# systemctl daemon-reload
-# systemctl enable jenkins
-# systemctl start jenkins
