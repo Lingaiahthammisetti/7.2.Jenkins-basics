6ba77a0c0fb04727dee5722591b6eab7233ea94e
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-08-26 19:22:23 +0530

    jenkins-msg

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,74 +1,68 @@
+# !/bin/bash
+# curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# yum install fontconfig java-17-openjdk jenkins -y
+# systemctl daemon-reload
+# systemctl enable jenkins
+# systemctl start jenkins
 
+!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-#!/bin/bash
-curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install fontconfig java-17-openjdk jenkins -y
-systemctl daemon-reload
-systemctl enable jenkins
-systemctl start jenkins
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo  -e "$G Script started executing at:$TIMESTAMP $N"
 
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+   echo -e "$R $2... FAITURE $N"
+   exit 1
+else
+   echo -e "$G $2..  SUCCESS $N"
+fi
+}
+#checking root user or not.
+if [ $USERID -ne 0 ]
+then
+   echo -e "$R Please run this script with root access $N"
+   exit 1
+else
+   echo -e "$G You are super user. $SCRIPT_NAME"
 
-# #!/bin/bash
-# USERID=$(id -u)
-# TIMESTAMP=$(date +%F-%H-%M-%S)
-# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-# LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+fi
 
-# R="\e[31m"
-# G="\e[32m"
-# Y="\e[33m"
-# N="\e[0m"
-# echo  -e "$G Script started executing at:$TIMESTAMP $N"
+yum update -y &>>$LOGFILE
+VALIDATE $? "Updating yum packages"
 
-# VALIDATE(){
-# if [ $1 -ne 0 ]
-# then 
-#    echo -e "$R $2... FAITURE $N"
-#    exit 1
-# else
-#    echo -e "$G $2..  SUCCESS $N"
-# fi
-# }
-# #checking root user or not.
-# if [ $USERID -ne 0 ]
-# then
-#    echo -e "$R Please run this script with root access $N"
-#    exit 1
-# else
-#    echo -e "$G You are super user. $SCRIPT_NAME"
+curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo &>>$LOGFILE
+VALIDATE $? "Add the LTS Jenkins repository"
 
-# fi
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key &>>$LOGFILE
+VALIDATE $? "Import the repo key"
 
-# yum update -y &>>$LOGFILE
-# VALIDATE $? "Updating yum packages"
+yum upgrade -y &>>$LOGFILE
+VALIDATE $? "Upgrading yum packages"
 
-# # curl -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo &>>$LOGFILE
-# # VALIDATE $? "Add the LTS Jenkins repository"
+sudo yum install fontconfig java-17-openjdk -y &>>$LOGFILE
+VALIDATE $? "Add required dependencies - Installing Java 17 openjdk"
 
-# rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key &>>$LOGFILE
-# VALIDATE $? "Import the repo key"
+yum install jenkins -y &>>$LOGFILE
+VALIDATE $? "Add required dependencies - Installing Jenkins"
 
-# yum upgrade -y &>>$LOGFILE
-# VALIDATE $? "Upgrading yum packages"
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Jenkins - Daemon reload"
 
-# sudo yum install fontconfig java-17-openjdk -y &>>$LOGFILE
-# VALIDATE $? "Add required dependencies - Installing Java 17 openjdk"
+systemctl enable jenkins &>>$LOGFILE
+VALIDATE $? "Enabling jenkins"
 
-# yum install jenkins -y &>>$LOGFILE
-# VALIDATE $? "Add required dependencies - Installing Jenkins"
+systemctl start jenkins &>>$LOGFILE
+VALIDATE $? "Starting jenkins"
 
-# systemctl daemon-reload &>>$LOGFILE
-# VALIDATE $? "Jenkins - Daemon reload"
-
-# systemctl enable jenkins &>>$LOGFILE
-# VALIDATE $? "Enabling jenkins"
-
-# systemctl start jenkins &>>$LOGFILE
-# VALIDATE $? "Starting jenkins"
-
-# systemctl status jenkins &>>$LOGFILE
-# VALIDATE $? "Checking Jenkins installation Status"
-
-# curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo &>>$LOGFILE
-# VALIDATE $? "downloading jenkins repository"
+systemctl status jenkins &>>$LOGFILE
+VALIDATE $? "Checking Jenkins installation Status"
